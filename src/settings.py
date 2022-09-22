acfcee06980b0e385049a70acd7f1eebd83429ae
JasonCacante <jason.cacante@gmail.com>
2022-09-21 21:00:27 -0500

    Adding pgadmin and making migrations

--- src/settings.py
+++ src/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x_bxtcfd!9d+wb%0h(!=nxt_*l(5vo3nj28w&ph8henj^wy%kh'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
